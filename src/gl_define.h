487a4d220e95e06b93590adad97b58a3bbd69eb1
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-25 01:08:33 +0900

    GLFWの面倒くさいインクルードやstb_image.hのインクルードをするヘッダを追加。
    普通はしないほうがいいのだが、毎度これを書くのはさすがに骨が折れるので。

---
+++ src/gl_define.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// GLFWのヘッダ内で他のライブラリを取り込む
+#define GLFW_INCLUDE_GLEXT
+#define GLFW_INCLUDE_GLU
+
+#if defined(_MSC_VER)
+// Windows:GLEWをスタティックライブラリ形式で利用
+#define GLEW_STATIC
+#include <GL/glew.h>
+#endif
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#if defined(_MSC_VER)
+// Windows:外部ライブラリのリンク指定
+#if defined (_DEBUG)
+#pragma comment(lib, "glfw3d.lib")
+#pragma comment(lib, "glew32sd.lib")
+#else
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "glew32s.lib")
+#endif
+#pragma comment(lib, "OPENGL32.lib")
+#pragma comment(lib, "GLU32.lib")
+#endif
