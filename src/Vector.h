b06a28c65d72ce44a11ae8e3e0aa5331d531ba34
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-25 15:46:29 +0900

    Vectorクラスを作成。
    xとyの値を持つクラスです。
    メンバ変数とオペレータを宣言。

---
+++ src/Vector.h
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace BlueSignal
+{
+
+	class Vector2
+	{
+	public:
+		Vector2();
+		Vector2(float _x, float _y);
+
+		Vector2& Vector2::operator=(const Vector2& v);
+		Vector2& operator+();
+		Vector2& operator-();
+		Vector2& operator+=(const Vector2& v);
+		Vector2& operator-=(const Vector2& v);
+		Vector2& operator*=(float s);
+		Vector2& operator/=(float s);
+
+		float x;
+		float y;
+	};
+
+	Vector2 operator+(const Vector2& v1, const Vector2& v2);
+	Vector2 operator-(const Vector2& v1, const Vector2& v2);
+	float operator*(const Vector2& v1, const Vector2& v2);
+
+	Vector2 operator*(const Vector2& v, float s);
+	Vector2 operator*(float s, const Vector2& v);
+	Vector2 operator/(const Vector2& v, float s);
+}
