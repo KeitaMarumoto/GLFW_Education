a324c97587e9637a64ed3c1791a36fcec4b67b61
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-25 18:57:41 +0900

    カラーの４要素(RGBA)を一元管理するクラスです。

---
+++ src/Color.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace BS 
+{
+	class Color4 
+	{
+	public:
+		Color4()
+			:r(0),g(0),b(0),a(0)
+		{}
+		Color4(float _r, float _g, float _b, float _a)
+			:r(_r), g(_g), b(_b), a(_a)
+		{}
+
+		float r, g, b, a;
+	};
+}
