90786e5567a42d4205a2a6df2b0cf00927d18e80
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-25 14:44:48 +0900

    Textureクラスの処理をCPPとHに分けました。

---
+++ src/Texture.cpp
@@ -0,0 +1,50 @@
+#include "Texture.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+BlueSignal::Texture::Texture(const char * path_)
+{
+	int w, h;  //幅と高さ
+	int comp;  //３or４
+	unsigned char* image = stbi_load(path_, &w, &h, &comp, 0);
+
+	std::cout << "w : " << w << ", h : " << h << std::endl;
+
+	//テクスチャを作成
+	glGenTextures(1, &tex_id);
+	glBindTexture(GL_TEXTURE_2D, tex_id);
+	//テクスチャをOpenGLのメモリにコピー
+	GLint type = (comp == 3)
+		? GL_RGB
+		: GL_RGBA;
+
+	glTexImage2D(GL_TEXTURE_2D,
+				 0,                 // mipmapレベル
+				 type, w, h,        // 内部形式とサイズ
+				 0, type,           // 境界色、形式
+				 GL_UNSIGNED_BYTE,
+				 image);
+
+	//画像破棄
+	stbi_image_free(image);
+
+	glTexParameteri(GL_TEXTURE_2D,
+					GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,
+					GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+}
+
+BlueSignal::Texture::~Texture()
+{
+	glDeleteTextures(1, &tex_id);
+}
+
+void BlueSignal::Texture::bind()
+{
+	glBindTexture(GL_TEXTURE_2D, tex_id);
+}
+
+void BlueSignal::Texture::unbind()
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
--- src/Texture.h
+++ src/Texture.h
@@ -7,42 +7,10 @@ namespace BlueSignal {
 	class Texture
 	{
 	public:
-		Texture(const char* path_) 
-		{
-			int w, h;  //幅と高さ
-			int comp;  //３or４
-			unsigned char* image = stbi_load(path_, &w, &h, &comp, 0);
-
-			std::cout << "w : " << w << ", h : " << h << std::endl;
-
-			//テクスチャを作成
-			glGenTextures(1, &tex_id);
-			glBindTexture(GL_TEXTURE_2D, tex_id);
-			//テクスチャをOpenGLのメモリにコピー
-			GLint type = (comp == 3)
-				? GL_RGB
-				: GL_RGBA;
-
-			glTexImage2D(GL_TEXTURE_2D,
-						 0,                 // mipmapレベル
-						 type, w, h,        // 内部形式とサイズ
-						 0, type,           // 境界色、形式
-						 GL_UNSIGNED_BYTE,
-						 image);
-
-			//画像破棄
-			stbi_image_free(image);
-
-			glTexParameteri(GL_TEXTURE_2D,
-							GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D,
-							GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		}
-
-		void bind() 
-		{
-			glBindTexture(GL_TEXTURE_2D, tex_id);
-		}
+		Texture(const char* path_);
+		~Texture();
+		void bind();
+		void unbind();
 
 	private:
 		GLuint tex_id;
