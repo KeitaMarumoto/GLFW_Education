2a4c957833092c9ac64ef99ec434bdee981b7f54
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-25 21:23:19 +0900

    Draw系の関数の実装を定義。
    Line、ワイヤーフレームQuad、塗りつぶしQuad、テクスチャ描画。に対応しました。

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -12,19 +12,106 @@ void BS::Draw::drawPoint(const Vector2& _pos, float _size, const Color4& _col)
 	glColor4f(_col.r, _col.g, _col.b, _col.a);
 
 	glEnableClientState(GL_VERTEX_ARRAY);
+	
 	glDrawArrays(GL_POINTS, 0, 1);
+	
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void BS::Draw::drawLine()
+void BS::Draw::drawLine(const Vector2& _start, const Vector2& _end, float _line_width, const Color4& _col)
 {
+	GLfloat vtx[] =
+	{
+		_start.x, _start.y,
+		_end.x, _end.y
+	};
+
+	glVertexPointer(2, GL_FLOAT, 0, vtx);
+	glLineWidth(_line_width);
+	glColor4f(_col.r, _col.g, _col.b, _col.a);
+	
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glDrawArrays(GL_LINES, 0, 2);
+	
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void BS::Draw::drawQuad()
+void BS::Draw::drawFillQuad(const Vector2& _center, const Vector2& _size, const Color4 _col)
 {
+	GLfloat vtx[] =
+	{
+		_center.x - _size.x / 2, _center.y + _size.y / 2,
+		_center.x + _size.x / 2, _center.y + _size.y / 2,
+		_center.x - _size.x / 2, _center.y - _size.y / 2,
+		_center.x + _size.x / 2, _center.y - _size.y / 2
+	};
+
+	glVertexPointer(2, GL_FLOAT, 0, vtx);
+	glColor4f(_col.r, _col.g, _col.b, _col.a);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 1, 3);
+	
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void BS::Draw::drawTexture()
+void BS::Draw::drawQuad(const Vector2 & _center, const Vector2 & _size, float _line_width, const Color4 _col)
 {
+	GLfloat vtx[] =
+	{
+		_center.x - _size.x / 2, _center.y + _size.y / 2,
+		_center.x + _size.x / 2, _center.y + _size.y / 2,
+		_center.x - _size.x / 2, _center.y - _size.y / 2,
+		_center.x + _size.x / 2, _center.y - _size.y / 2
+	};
 
+	glVertexPointer(2, GL_FLOAT, 0, vtx);
+	glLineWidth(_line_width);
+	glColor4f(_col.r, _col.g, _col.b, _col.a);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glDrawArrays(GL_LINE_LOOP, 0, 3);
+	glDrawArrays(GL_LINE_LOOP, 1, 3);
+	
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+void BS::Draw::drawTexture(const Texture& _texture, const Vector2& _center, const Vector2& _size, const Color4 _col)
+{
+	GLfloat vtx[] =
+	{
+		_center.x - _size.x / 2, _center.y + _size.y / 2,
+		_center.x + _size.x / 2, _center.y + _size.y / 2,
+		_center.x - _size.x / 2, _center.y - _size.y / 2,
+		_center.x + _size.x / 2, _center.y - _size.y / 2
+	};
+
+	GLfloat uv[] = 
+	{
+		0.0, 0.0,
+		1.0, 0.0,
+		0.0, 1.0,
+		1.0, 1.0
+	};
+
+	glVertexPointer(2, GL_FLOAT, 0, vtx);
+	glTexCoordPointer(2, GL_FLOAT, 0, uv);
+	glColor4f(_col.r, _col.g, _col.b, _col.a);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnable(GL_TEXTURE_2D);
+
+	_texture.bind();
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 1, 3);
+	_texture.unbind();
+
+	glDisable(GL_TEXTURE_2D);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
--- src/Draw.h
+++ src/Draw.h
@@ -3,14 +3,16 @@
 #include "Vector.h"
 #include "Color.h"
 #include "gl_define.h"
+#include "Texture.h"
 
 namespace BS {
 
 	namespace Draw
 	{
 		void drawPoint(const Vector2& _pos, float _size, const Color4& _col);
-		void drawLine();
-		void drawQuad();
-		void drawTexture();
+		void drawLine(const Vector2& _start,const Vector2& _end, float _line_width, const Color4& _col);
+		void drawFillQuad(const Vector2& _center, const Vector2& _size, const Color4 _col);
+		void drawQuad(const Vector2& _center, const Vector2& _size, float _line_width, const Color4 _col);
+		void drawTexture(const Texture& _texture, const Vector2& _center, const Vector2& _size, const Color4 _col);
 	}
 }
