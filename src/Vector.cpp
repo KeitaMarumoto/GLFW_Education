62081c94331dcc3acde59d9c3ba04939207e1c0f
KeitaMarumoto <marumoto.keita@gmail.com>
2017-07-27 18:20:02 +0900

    Vector3一応完成。
    Vector3のオペレータを定義。
    Vector2とVector3にdot()とcross()を追加。

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -79,6 +79,11 @@ BS::Vector2 BS::operator/(const Vector2 & v, float s)
 // Vector3
 //*************************************************************************
 
+inline BS::Vector3 & BS::Vector3::cross(const Vector3 & v1, const Vector3 & v2)
+{
+	return Vector3(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+}
+
 BS::Vector3 & BS::Vector3::operator=(const Vector3 & v)
 {
 	this->x = v.x;
@@ -103,6 +108,21 @@ float BS::operator*(const Vector3 & v1, const Vector3 & v2)
 	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
+BS::Vector3 BS::operator*(const Vector3 & v, float s)
+{
+	return Vector3(v.x*s, v.y*s, v.z*s);
+}
+
+BS::Vector3 BS::operator*(float s, const Vector3 & v)
+{
+	return Vector3(v.x*s, v.y*s, v.z*s);
+}
+
+BS::Vector3 BS::operator/(const Vector3 & v, float s)
+{
+	return Vector3(v.x / s, v.y / s, v.z / s);
+}
+
 BS::Vector3 & BS::Vector3::operator+()
 {
 	return *this;
@@ -115,20 +135,32 @@ BS::Vector3 & BS::Vector3::operator-()
 
 BS::Vector3 & BS::Vector3::operator+=(const Vector3 & v)
 {
-	// TODO: return ステートメントをここに挿入します
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
 }
 
 BS::Vector3 & BS::Vector3::operator-=(const Vector3 & v)
 {
-	// TODO: return ステートメントをここに挿入します
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	return *this;
 }
 
 BS::Vector3 & BS::Vector3::operator*=(float s)
 {
-	// TODO: return ステートメントをここに挿入します
+	x *= s;
+	y *= s;
+	z *= s;
+	return *this;
 }
 
 BS::Vector3 & BS::Vector3::operator/=(float s)
 {
-	// TODO: return ステートメントをここに挿入します
+	x /= s;
+	y /= s;
+	z /= s;
+	return *this;
 }
